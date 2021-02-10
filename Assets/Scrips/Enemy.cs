f03c171508bc7b98ce512255eb41b6dc137d1c74
JoshuahBoardman <joshuahboardman@gmail.com>
2021-02-09 21:10:41 -0600

    Added fire enemy patrols

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -4,23 +4,73 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
-    [SerializeField] float climbSpeed = 5;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] float turnRate = 5f;
 
-    private float randomNumber;
+    [SerializeField] private Transform groundCheckR;
+    [SerializeField] private Transform groundCheckL;
+
+    private int randomNumber;
+
+    private bool isGrounded;
+    private bool moveRight;
+
+    private Rigidbody2D rigidbody;
 
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Patrol", 0f, turnRate);
     }
 
     private void FixedUpdate()
     {
-        
+        GrounDetection();
+        LedgeSave();
     }
 
-    private void Randomness()
+    void Patrol()
     {
-        randomNumber = Random.Range(1f, 10f);
+        randomNumber = Random.Range(1, 10);
+        Debug.Log(randomNumber);
+
+        if (randomNumber % 2f == 0)
+        {
+            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+            moveRight = true;
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+            moveRight = false;
+        }
+
+    }
+    private void LedgeSave()
+    {
+        if (!isGrounded)
+        {
+            if(moveRight == false)
+            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+            else
+            {
+                rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+            }
+        
+        }
+    }
+
+    private void GrounDetection()
+    {
+        if (Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")) && 
+            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
